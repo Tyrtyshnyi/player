0b82453a28923c7efadf253e1591a9e7ecba81c5
lowgenre <Dmitryi.darya@gmail.com>
2024-10-19 20:16:00 +0300

    2

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const { app, BrowserWindow } = require('electron');
 
+require('electron-reload')(__dirname, {
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+
+
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
